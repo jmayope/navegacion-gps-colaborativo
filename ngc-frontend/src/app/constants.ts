a4fe135e73d717ce611c7db0cd81b677208bc1a4
Jorge Mayo Calderon <lookmmm@gmail.com>
2026-08-27 17:57:13 -0500

    Agregando intefaz de login y registro con BD

---
+++ ngc-frontend/src/app/constants.ts
@@ -0,0 +1,68 @@
+import Swal from "sweetalert2";
+
+export const API_URI = "http://localhost:8080/api";
+export const TOKEN_NAME = 'NGS-TOKEN';
+
+
+export function generateRandomString(length: number) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+export function loadingAlert(message: string, timerInSeconds?: number) {
+  if (timerInSeconds) {
+    Swal.fire({
+      html: `
+        <div class="spinner-grow text-primary" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+        <div class="spinner-grow text-secondary" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+        <div class="spinner-grow text-success" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div><br>${message}
+      `,
+      timer: timerInSeconds * 1000,
+      allowEscapeKey: false,
+      allowOutsideClick: false,
+      showConfirmButton: false,
+      showCancelButton: false
+    });
+  } else {
+    Swal.fire({
+      html: `
+        <div class="spinner-grow text-primary" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+        <div class="spinner-grow text-secondary" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+        <div class="spinner-grow text-success" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div><br>${message}
+      `,
+      allowEscapeKey: false,
+      allowOutsideClick: false,
+      showConfirmButton: false,
+      showCancelButton: false
+    });
+  }
+}
+
+export function messageAlert(title: any, message: string, icon: string) {
+  let icons: any = {
+    'success': 'success',
+    'error': 'error',
+    'warning': 'warning',
+    'info': 'info',
+  }
+  Swal.fire({
+    text: message,
+    icon: icons[icon]
+  });
+}
